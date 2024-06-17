d269e92fdb6034f267cb76b2301375f8399d54bc
kalandhar <kalandars2004@gmail.com>
2024-06-17 17:47:39 +0530

    first commit

---
+++ 3C/FileClient.java
@@ -0,0 +1,25 @@
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+public class FileClient
+{
+public static void main(String[] args) throws Exception
+{
+//Initialize socket
+Socket socket = new Socket(InetAddress.getByName("localhost"), 5000);
+byte[] contents = new byte[10000];
+
+FileOutputStream fos = new FileOutputStream("G:\\My Drive\\CN\\3C\\text.txt");
+BufferedOutputStream bos = new BufferedOutputStream(fos);
+InputStream is = socket.getInputStream();
+//No of bytes read in one read() call
+int bytesRead = 0;
+while((bytesRead=is.read(contents))!=-1)
+bos.write(contents, 0, bytesRead);
+bos.flush();
+socket.close();
+System.out.println("File saved successfully!");
+}
+}
---
+++ 5A ARP/Server.java
@@ -0,0 +1,34 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+class Server {
+    public static void main(String args[]) {
+        try {
+            ServerSocket obj = new ServerSocket(9999);
+            Socket obj1 = obj.accept();
+
+            while (true) {
+                DataInputStream din = new DataInputStream(obj1.getInputStream());
+                DataOutputStream dout = new DataOutputStream(obj1.getOutputStream());
+
+                String str = din.readLine();
+
+                String ip[] = {"165.165.80.80", "165.165.79.1"};
+                String mac[] = {"6A:08:AA:C2", "8A:BC:E3:FA"};
+
+                for (int i = 0; i < ip.length; i++) {
+                    if (str.equals(ip[i])) {
+                        dout.writeBytes(mac[i] + '\n');
+                        break;
+                    }
+                }
+
+                obj.close(); 
+                    }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
